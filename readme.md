25a88fc08785a6135ff1acda658a130f24ebea89
Alex Coté <alex.cote@tuta.io>
2021-11-04 01:01:37 -0500

    Smooth gradient; cleaned up code/repo

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Mandelbrot Set in Rust and SDL
+
+![Image of Mandelbrot render](https://raw.githubusercontent.com/quot/Rust-Mandelbrot/main/src/screenshot.png)
+
+This was made as a way to practice Rust. Most of the equations and structure have been pulled from external sources.
+
+Original project that this was based on: https://github.com/rust-in-action/code/blob/1st-edition/ch2/ch2-mandelbrot/src/main.rs
+
+Equations for smooth gradient: https://www.reddit.com/r/math/comments/2abwyt/smooth_colour_mandelbrot/citjvpv/
--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,66 @@
 extern crate sdl2;
 
-use num::ToPrimitive;
-use sdl2::pixels::Color;
+use num::complex::Complex;
+use num::{Float, ToPrimitive};
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::time::Duration;
-use std::cmp::min;
+use sdl2::pixels::Color;
 use sdl2::rect::Point;
-use num::complex::Complex;
+use std::cmp::min;
+use std::time::Duration;
 
-fn mandelbrot_value_at_point(
-    cx: f64,
-    cy: f64,
-    max_iters: i32
-) -> i32 {
+fn mandelbrot_color_at_point(cx: f64, cy: f64, max_iters: usize) -> Color {
     let mut z = Complex { re: 0.0, im: 0.0 };
     let c = Complex::new(cx, cy);
 
     for i in 0..=max_iters {
-        if z.norm() > 20.0 {
-            return i;
+        if z.norm() > 50.0 {
+            let mut v: f64 = (i as f64 + 1.5 - z.norm().abs().log10().log2()).log10() / 3.4;
+            if v < 1.0 {
+                return Color::RGB(
+                    min(255, (255.0 * v.powf(4.0)).round() as u8),
+                    min(255, (255.0 * v.powf(2.5)).round() as u8),
+                    min(255, (255.0 * v).round() as u8),
+                );
+            } else {
+                v = (2.0 - v).max(0.0);
+                return Color::RGB(
+                    min(255, (255.0 * v).round() as u8),
+                    min(255, (255.0 * v.powf(1.5)).round() as u8),
+                    min(255, (255.0 * v.powf(3.0)).round() as u8),
+                );
+            }
         }
         z = z * z + c;
     }
-    max_iters
+    Color::RGB(0, 0, 0)
 }
 
 pub fn main() {
-    let mandl_x_min: f64 = -2.0;
-    let mandl_x_max: f64 = 1.0;
-    let mandl_y_min: f64 = -1.0;
-    let mandl_y_max: f64 = 1.0;
-    let win_width: u32 = 800;
-    let win_height: u32 = 600;
-    let max_iters: i32 = 1000;
+    let win_width: u32 = 1920;
+    let win_height: u32 = 1300;
+    let mandl_x_min: f64 = -2.1;
+    let mandl_x_max: f64 = 0.8;
+    let mandl_y_min: f64 =
+        ((mandl_x_min - mandl_x_max) * 0.5 * win_height as f64) / win_width as f64;
+    let mandl_y_max: f64 = (0.0 - mandl_y_min as f64) + 0.01;
+    let max_iters: usize = 500;
 
     //===========
     // SDL Setup
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
-
-    let window = video_subsystem.window("Mandelbrot", win_width, win_height)
+    let window = video_subsystem
+        .window("Mandelbrot", win_width, win_height)
         .position_centered()
         .build()
         .unwrap();
-
     let mut canvas = window.into_canvas().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
 
     canvas.set_draw_color(Color::RGB(0, 0, 0));
     canvas.clear();
     canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
 
     //==============
     // Drawing Loop
@@ -59,10 +69,11 @@ pub fn main() {
         // Check for quit events
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             }
         }
@@ -76,28 +87,12 @@ pub fn main() {
                 let y_percent = cur_y as f64 / win_height as f64;
                 let cx = mandl_x_min + (mandl_x_max - mandl_x_min) * x_percent;
                 let cy = mandl_y_min + (mandl_y_max - mandl_y_min) * y_percent;
-                let mandelbrot_val = mandelbrot_value_at_point(cx, cy, max_iters);
-                //if (mandelbrot_val < 1000 && mandelbrot_val > 0) { println!("MandVal: {}", mandelbrot_val) }
 
-                let rgb_vals: Color = match mandelbrot_val {
-                    0 => Color::RGB(0, 0, 0),
-                    1 => Color::RGB(0, 0, 50),
-                    2 => Color::RGB(0, 0, 100),
-                    3 => Color::RGB(0, 0, 150),
-                    4 => Color::RGB(0, 0, 200),
-                    5 => Color::RGB(0, 0, 220),
-                    6..=7 => Color::RGB(0, 0, 255),
-                    8..=9 => Color::RGB(20, 20, 255),
-                    10..=20 => Color::RGB(40, 40, 255),
-                    21..=30 => Color::RGB(70, 70, 255),
-                    31..=50 => Color::RGB(100, 100, 255),
-                    51..=100 => Color::RGB(150, 150, 255),
-                    101..=999 => Color::RGB(200, 200, 255),
-                    _ => Color::RGB(255,255,255)
-                };
                 // Draw calculated value
-                canvas.set_draw_color(rgb_vals);
-                canvas.draw_point(Point::new(cur_x.to_i32().unwrap(), cur_y.to_i32().unwrap()));
+                canvas.set_draw_color(mandelbrot_color_at_point(cx, cy, max_iters));
+                canvas
+                    .draw_point(Point::new(cur_x.to_i32().unwrap(), cur_y.to_i32().unwrap()))
+                    .unwrap();
             }
             cur_y += 1;
         }
